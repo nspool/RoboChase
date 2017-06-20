6d2406fafd6e61b25d5a086c54a561969d0e93bd
nspool <nspool@gmx.com>
2017-06-20 20:27:34 +1000

    Player animations when running east and west

--- RoboChase/Player.cpp
+++ RoboChase/Player.cpp
@@ -28,6 +28,21 @@ Player::Player(SDL_Renderer* renderer, SDL_Point p)
   spriteClips_[1].y = 0;
   spriteClips_[1].w = 21;
   spriteClips_[1].h = 31;
+
+  spriteClips_[2].x = 42;
+  spriteClips_[2].y = 0;
+  spriteClips_[2].w = 21;
+  spriteClips_[2].h = 31;
+  
+  spriteClips_[3].x = 63;
+  spriteClips_[3].y = 0;
+  spriteClips_[3].w = 21;
+  spriteClips_[3].h = 31;
+  
+  spriteClips_[4].x = 85;
+  spriteClips_[4].y = 0;
+  spriteClips_[4].w = 21;
+  spriteClips_[4].h = 31;
   
   texture_ = SDL_CreateTextureFromSurface(renderer, gPlayer);
 }
@@ -40,16 +55,16 @@ void Player::action(SDL_Point* target, std::vector<SDL_Rect>* obsticles)
   if(!xd && !yd) {
     direction_ = x;
   } else {
-    if(yd > 0) { direction_ = w; }
-    if(yd < 0) { direction_ = e; }
-    if(xd > 0) { direction_ = s; }
-    if(xd < 0) { direction_ = n; }
+    if(xd < 0) { direction_ = w; }
+    if(xd > 0) { direction_ = e; }
+//    if(xd > 0) { direction_ = s; }
+//    if(xd < 0) { direction_ = n; }
 
-    if(yd > 0 && xd > 0) { direction_ = sw; }
-    if(yd < 0 && xd < 0) { direction_ = ne; }
-    if(yd > 0 && xd < 0) { direction_ = nw; }
-    if(yd < 0 && xd > 0) { direction_ = se; }
-        
+//    if(yd > 0 && xd > 0) { direction_ = sw; }
+//    if(yd < 0 && xd < 0) { direction_ = ne; }
+//    if(yd > 0 && xd < 0) { direction_ = nw; }
+//    if(yd < 0 && xd > 0) { direction_ = se; }
+    
     position_.x = target->x;
     position_.y = target->y;
   }
@@ -61,12 +76,14 @@ void Player::render(int ticks)
   constexpr int animationRate = 12;
   constexpr int animationLen = 2;
   
+  
   SDL_Rect bounds = getBounds();
   
   if(direction_ != x){
+    int offset = (direction_ == e) ? 1 : 3;
     int frameToDraw = (ticks * animationRate / 1000) % animationLen;
     SDL_Rect bounds = getBounds();
-    SDL_RenderCopy(renderer_, texture_, &spriteClips_[frameToDraw], &bounds);
+    SDL_RenderCopy(renderer_, texture_, &spriteClips_[frameToDraw + offset], &bounds);
   } else {
     SDL_RenderCopy(renderer_, texture_, &spriteClips_[0], &bounds );
   }
